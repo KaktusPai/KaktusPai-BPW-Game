f5ccd3122965fc63053be5de2dbf27b62aa743bb
KaktusPai <jessebrolsma@gmail.com>
2020-03-27 16:25:23 +0100

    Added base game with early stage platformtoggle mechanic and working double jump mechanic

---
+++ BPW-Game/Assets/Platform2Toggle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform2Toggle
+    : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            GetComponent<Collider>().enabled = !GetComponent<Collider>().enabled;
+        }
+    }
+}
---
+++ BPW-Game/Assets/Respawn.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public Transform player;
+    public Transform respawnPoint;
+
+    void onTriggerEnter(Collider other)
+    {
+        player.transform.position = respawnPoint.transform.position;
+        Debug.Log("YOOOOOOOOOOOOOO");
+    }
+}
